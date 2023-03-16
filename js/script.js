355e10d5436ae87c0eab2afc2f0fe9fec6e40dd9
taddeoneri <taddeo.neri10@gmail.com>
2023-03-16 16:32:15 +0100

    init even odd

--- js/script.js
+++ js/script.js
@@ -18,8 +18,48 @@ btnReverse.addEventListener('click', function(e){
 })
 
 function reverseString(str) {
-    let splitString = str.split(""); 
-    let reverseArray = splitString.reverse();
-    let joinArray = reverseArray.join(""); 
+    const splitString = str.split(""); 
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join(""); 
     return joinArray;
+}
+
+
+
+/* 
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/      
+
+
+const btnNumber2 = document.querySelector('.btnNumber2');
+const btnWinner = document.querySelector('.btnWinner');
+
+let number1;
+let number2;
+
+
+
+btnNumber2.addEventListener('click', function(){
+    number2 = getRandomInt(1, 6);
+    return document.querySelector('.computer').innerHTML += 
+    `
+        <p class="px-4 mb-0">${number2}</p>
+    `;
+});
+
+
+btnWinner.addEventListener('click', function(){
+    number1 = document.getElementById('numberUser').value;
+    console.log(number1);
+})
+
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
